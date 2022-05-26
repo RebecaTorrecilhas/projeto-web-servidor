f10daabd9cbc1ec793402a0a23abea61c0ee336d
RebecaTorrecilhas <rebeca.torrecilhas@gmail.com>
2022-05-25 21:52:27 -0300

    add: composer, edit: editar perfil, add imgs

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require "vendor/autoload.php";
 $path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] :  '';
 
 $class = 'IndexController';
--- view/editar-perfil.view.php
+++ view/editar-perfil.view.php
@@ -9,6 +9,8 @@
 	<link rel="stylesheet" href="/public/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/public/css/config.css">
 	<link rel="stylesheet" href="/public/css/editar-perfil.css">
+	<link rel="stylesheet" href="/public/css/recuperar-senha.css">
+
 </head>
 
 <body>
@@ -19,8 +21,64 @@
 	</header>
 
 	<div class="container">
-		<p class="regular-16 color-gray-500">Olá, <?php echo $_SESSION['nome']; ?></p>
+		<div class="card bg-gray-200">
+			<p class="regular-16 color-gray-500">Olá, <?php echo $_SESSION['nome']; ?></p>
+
+			<form method="POST" action="/auth/cadastrar">
+
+				<div class="container">
+					<button type="button" onClick="addImage()" class="button-no-style">
+						<div class="img-perfil" id="image" />
+					</button>
+					<input type="file" name="photo" hidden id="photo" />
+				</div>
+				<div class="p-2 text-center">
+					<b class="regular-16 color-gray-500"> Clique na imagem para alterar </b>
+				</div>
+
+				<div class="text-field">
+					<input type="text" name="nome" required placeholder="Nome completo" />
+					<p class="regular-12 color-error"><?php echo $this->errors['nome'] ?? '' ?></p>
+				</div>
+
+				<div class="text-field">
+					<input type="email" name="email" required placeholder="E-mail" />
+					<p class="regular-12 color-error"><?php echo $this->errors['email'] ?? '' ?></p>
+				</div>
+
+				<div class="text-field">
+					<input type="password" name="password" required placeholder="Senha" />
+					<p class="regular-12 color-error"><?php echo $this->errors['password'] ?? '' ?></p>
+				</div>
+
+				<div class="text-field">
+					<input type="password" name="confirm-password" required placeholder="Confirmar Senha" />
+					<p class="regular-12 color-error"><?php echo $this->errors['confirm-password'] ?? '' ?></p>
+				</div>
+			</form>
+
+			<button type="submit" class="button-primary">Editar Cadastro</button>
+		</div>
 	</div>
 </body>
+<script>
+	addImage = () => {
+		document.getElementById("photo").click();
+	}
+
+	function changeImage() {
+		console.log(this.files);
+		if (this.files && this.files[0]) {
+			const blob = new Blob([this.files[0]]);
+			const url = URL.createObjectURL(blob, {
+				type: "image/png"
+			});
+			document.getElementById("image").style.backgroundImage = `url("${url}")`;
+
+		}
+	}
+
+	document.getElementById("photo").addEventListener("change", changeImage, false);
+</script>
 
 </html>
