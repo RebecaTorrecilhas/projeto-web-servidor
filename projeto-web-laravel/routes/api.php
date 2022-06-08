b936a3e1fc2545773b258755af50562d7008f3e6
RebecaTorrecilhas <rebeca.torrecilhas@gmail.com>
2022-06-08 20:08:09 -0300

    controllers, router

--- projeto-web-laravel/routes/api.php
+++ projeto-web-laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AutenticacaoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AutenticacaoController::class)->group(function () {
+    Route::post('/autenticacao', 'login');
+    Route::post('/autenticacao/recuperar-senha', 'forgotPassword');
+    Route::post('/autenticacao/alterar-senha', 'changePassword');
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
 });
