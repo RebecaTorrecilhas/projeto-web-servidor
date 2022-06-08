61bbb5e553fd05fa870c052197f01f87e2c87b71
RebecaTorrecilhas <rebeca.torrecilhas@gmail.com>
2022-06-08 19:05:18 -0300

    laravel

--- projeto-web-php/utils/DB.php
+++ projeto-web-php/utils/DB.php
@@ -5,7 +5,7 @@ class DB {
 		//require_once 'utils/Constants.php';
 
 		try {
-			return new PDO("mysql:host=localhost;port=3306;dbname=web_servidor;", 'root', '1234');
+			return new PDO("mysql:host=localhost;port=3306;dbname=web_servidor;", 'root', '');
 			// return new PDO("mysql:host={$db->host};port={$db->port};dbname={$db->database};", $db->username, $db->password);
 		} catch (PDFException $e) {
 			die($e->getMessage());
